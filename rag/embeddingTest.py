fa3042408e2517a63c66e8c0cd549f30d3500cc8
menglin008 <menglin008@ke.com>
2025-06-17 19:26:33 +0800

    u

--- rag/embeddingTest.py
+++ rag/embeddingTest.py
@@ -1,4 +1,5 @@
 import math
+from sentence_transformers import SentenceTransformer
 
 def dot_product(a, b):
     return sum(x*y for x, y in zip(a, b))
@@ -11,18 +12,17 @@ def cosine_similarity(a, b):
 
 vec1 = [1, 2, 3]
 vec2 = [4, 5, 6]
-
-print("Cosine Similarity:", cosine_similarity(vec1, vec2))
-
-from sentence_transformers import SentenceTransformer
-
+print("66")
 model = SentenceTransformer('paraphrase-MiniLM-L6-v2')
-
+print("77")
 sent1 = "I like machine learning."
 sent2 = "I enjoy studying AI."
 
 emb1 = model.encode(sent1)
+
+print(emb1.shape)
 emb2 = model.encode(sent2)
 
 similarity = cosine_similarity(emb1, emb2)
+print("99999")
 print(f"相似度：{similarity:.4f}")
--- transformer_example/data.py
+++ transformer_example/data.py
@@ -1,17 +1,21 @@
-# 定义字典
-zidian_x = '<SOS>,<EOS>,<PAD>,0,1,2,3,4,5,6,7,8,9,q,w,e,r,t,y,u,i,o,p,a,s,d,f,g,h,j,k,l,z,x,c,v,b,n,m'
-zidian_x = {word: i for i, word in enumerate(zidian_x.split(','))}
-
-zidian_xr = [k for k, v in zidian_x.items()]
-
-zidian_y = {k.upper(): v for k, v in zidian_x.items()}
-
-zidian_yr = [k for k, v in zidian_y.items()]
-
 import random
 
 import numpy as np
 import torch
+
+# 定义字典
+zidian_x = '<SOS>,<EOS>,<PAD>,0,1,2,3,4,5,6,7,8,9,q,w,e,r,t,y,u,i,o,p,a,s,d,f,g,h,j,k,l,z,x,c,v,b,n,m'
+zidian_x = {word: i for i, word in enumerate(zidian_x.split(','))}
+print(zidian_x)
+
+zidian_xr = [k for k, v in zidian_x.items()]
+print(zidian_xr)
+
+zidian_y = {k.upper(): v for k, v in zidian_x.items()}
+print(zidian_y)
+
+zidian_yr = [k for k, v in zidian_y.items()]
+print(zidian_yr)
 
 
 def get_data():
